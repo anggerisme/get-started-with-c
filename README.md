359f38142bf7696e35660959cddc17c616249f89
Angger Bayu Pangestu <kontak@angger.me>
2021-03-22 14:54:48 +0700

    :pencil: add materi : variable & printf :package:

--- README.md
+++ README.md
@@ -24,3 +24,24 @@ int main()
     return 0;
 }
 ```
+
+# The power of variable & printf 🔥
+
+```c
+int main()
+{
+    int umur = 20;
+    char nama1[] = "Budi";
+    char nama2[] = "Andi";
+    float ipk = 3.7532f;
+    double phi = 3.14159265358979323846;
+    printf("%s berumur %i\n", nama1, umur);
+    printf("%s berumur %i\n", nama2, umur+1);
+    printf("Nilai ipk %s adalah %.3f \n", nama1, ipk); // %.3 Untuk menentukan nilai di belakang koma
+    printf("Nilai phi adalah %.10f \n", phi);
+    printf("%s %s %i\n", nama1, "berumur", umur+1);
+}
+```
+
+`$` digunakan sebagai _placeholder_ untuk menaruh variable yang sudah dideklarasikan diawal. `$d` untuk angka dan `$s`.
+
--- main.c
+++ main.c
@@ -11,12 +11,39 @@
 
 // Drawing cool shape
 
+// int main()
+// {
+//     printf("     /|\n");
+//     printf("    / |\n");
+//     printf("   /  |\n");
+//     printf("  /   |\n");
+//     printf(" /____|\n");
+//     return 0;
+// }
+
+// Variable & Tipe Data
+
 int main()
 {
-    printf("     /|\n");
-    printf("    / |\n");
-    printf("   /  |\n");
-    printf("  /   |\n");
-    printf(" /____|\n");
+    int umur = 20;
+    char nama1[] = "Budi";
+    char nama2[] = "Andi";
+    float ipk = 3.7532f;
+    double phi = 3.14159265358979323846;
+    printf("%s berumur %i\n", nama1, umur);
+    printf("%s berumur %i\n", nama2, umur+1);
+    printf("Nilai ipk %s adalah %.3f \n", nama1, ipk); // %.3 Untuk menentukan nilai di belakang koma
+    printf("Nilai phi adalah %.10f \n", phi);
+    printf("%s %s %i\n", nama1, "berumur", umur+1);
     return 0;
 }
+
+// Tipe Data
+
+// int main()
+// {
+//     float ipk = 3.14f;
+//     printf("Nilai ipk %f ")
+// }
+
+// Float : hanya untuk angka desimal berukuran kecil. Tipe data float tidak dapat menyimpan data pembagian yang nilai dibelakang koma tidak habis (tak berhingga) seperti 22/7
