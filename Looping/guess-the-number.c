03884d41dd73ff35d47563b07f4f1aafaa3a7f7b
Angger Bayu Pangestu <kontak@angger.me>
2021-03-24 20:22:58 +0700

    :pencil: add materi : Build the guess my number game! #2 :black_joker:

--- Looping/guess-the-number.c
+++ Looping/guess-the-number.c
@@ -1,16 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+///////// Build the guess my number game! #1 //////////
+
+// int main()
+// {
+//     int guess;
+//     int secretNumber = 5;
+//     while(guess != secretNumber)
+//     {
+//         printf("Silahkan tebak angka :\n");
+//         scanf("%d", &guess);
+
+//     }
+//     printf("Anda benar! Angkanya adalah %d", secretNumber);
+//     return 0;
+// }
+
+///////// Build the guess my number game! #2 //////////
 int main()
 {
     int guess;
     int secretNumber = 5;
-    while(guess != secretNumber)
+    int guessLimit = 3;
+    int guessCount = 0;
+    int outOfGuess = 0;
+    while(guess != secretNumber && outOfGuess == 0)
     {
-        printf("Silahkan tebak angka :\n");
-        scanf("%d", &guess);
+        if (guessCount < guessLimit){
+            printf("Silahkan tebak angka :\n");
+            scanf("%d", &guess);
+            guessCount++;
+        }
+        else {
+            outOfGuess = 1; // Isikan outOfGuess dengan 1 untuk menyatakan salah
+        }
 
     }
-    printf("Anda benar! Angkanya adalah %d", secretNumber);
+    if (outOfGuess == 1){
+        printf("Anda kalah!");
+    }
+    else {
+        printf("Anda benar!");
+    }
+
     return 0;
 }
