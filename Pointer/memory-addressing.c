32a49a9182db0c5f5df2796d5b18440ad9da2090
Angger Bayu Pangestu <kontak@angger.me>
2021-03-25 14:16:56 +0700

    :pencil: add file : memory-addressing :pencil:

---
+++ Pointer/memory-addressing.c
@@ -0,0 +1,26 @@
+/*----- Mengakses lokasi memory -----*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float f = 3.14;
+    int age = 20;
+    printf("f : %p, age : %p", &f, &age);
+
+    return 0;
+}
+
+
+
+
+
+// int main()
+// {
+//     int age = 20;
+//     double ipk = 3.75;
+//     char grade = 'A';
+
+//     return 0;
+// }
